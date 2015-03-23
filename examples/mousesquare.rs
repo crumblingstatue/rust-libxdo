442f7fe366efd1f985fa80cf0150c4ca06febaaf
Mika Attila <radiantstatue@gmail.com>
2015-03-23 14:25:31 +0100

    Add move_mouse_relative method

---
+++ examples/mousesquare.rs
@@ -0,0 +1,21 @@
+#![feature(std_misc, thread_sleep)]
+
+extern crate libxdo;
+
+use libxdo::XDo;
+use std::time::Duration;
+
+fn move_it(xdo: &XDo, rel_x: i32, rel_y: i32, times: i32) {
+    for _ in 0..times {
+        xdo.move_mouse_relative(rel_x, rel_y, 0).unwrap();
+        std::thread::sleep(Duration::milliseconds(10));
+    }
+}
+
+fn main() {
+    let xdo = XDo::new(None).unwrap();
+    move_it(&xdo, 10, 0, 20);
+    move_it(&xdo, 0, 10, 20);
+    move_it(&xdo, -10, 0, 20);
+    move_it(&xdo, 0, -10, 20);
+}

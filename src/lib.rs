1b0d141bb1f48767205ac31b86512823ac625a2e
Mika Attila <radiantstatue@gmail.com>
2015-03-23 17:14:25 +0100

    Add rough documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+//! High level bindings to [libxdo](http://www.semicomplete.com/files/xdotool/docs/html/)
+
 #![feature(std_misc)]
+#![warn(missing_docs)]
 
 extern crate "libxdo-sys" as sys;
 extern crate libc;
@@ -9,13 +12,18 @@ use libc::c_int;
 use std::ptr::null;
 use std::time::Duration;
 
+
+/// An XDo instance
 pub struct XDo {
     handle: *mut sys::xdo
 }
 
+/// An error that can happen when trying to create an XDo instance.
 #[derive(Debug)]
 pub enum XDoCreationError {
+    /// The parameter passed in contained a nul character.
     NulError(NulError),
+    /// Unknown error
     Unknown
 }
 
@@ -32,6 +40,7 @@ enum XDoOperationErrorKind {
     OperationFailed
 }
 
+/// An error originating from an XDo operation.
 #[derive(Debug)]
 struct XDoOperationError {
     kind: XDoOperationErrorKind
@@ -45,6 +54,7 @@ impl FromError<NulError> for XDoOperationError {
     }
 }
 
+/// Result of an XDo operation.
 pub type OpResult = Result<(), XDoOperationError>;
 
 macro_rules! xdo (
@@ -78,6 +88,16 @@ macro_rules! try_microsecs (
 );
 
 impl XDo {
+    /// Creates a new XDo instance.
+    ///
+    /// # Parameters
+    ///
+    /// display - An optional string display name, such as ":0". If None, uses the environment
+    /// DISPLAY.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new XDo instance, or an XDoCreationError on error.
     pub fn new(display: Option<&str>) -> Result<XDo, XDoCreationError> {
         let display = match display {
             Some(display) => {
@@ -94,38 +114,47 @@ impl XDo {
             handle: handle
         })
     }
+    /// Moves the mouse to the specified position.
     pub fn move_mouse(&self, x: i32, y: i32, screen: i32) -> OpResult {
         xdo!(sys::xdo_mousemove(self.handle, x as c_int, y as c_int, screen as c_int))
     }
+    /// Moves the mouse relative to the current position.
     pub fn move_mouse_relative(&self, x: i32, y: i32) -> OpResult {
         xdo!(sys::xdo_mousemove_relative(self.handle, x as c_int, y as c_int))
     }
+    /// Does a mouse click.
     pub fn click(&self, button: i32) -> OpResult {
         xdo!(sys::xdo_click(self.handle, sys::CURRENTWINDOW, button as c_int))
     }
+    /// Holds a mouse button down.
     pub fn mouse_down(&self, button: i32) -> OpResult {
         xdo!(sys::xdo_mousedown(self.handle, sys::CURRENTWINDOW, button as c_int))
     }
+    /// Releases a mouse button.
     pub fn mouse_up(&self, button: i32) -> OpResult {
         xdo!(sys::xdo_mouseup(self.handle, sys::CURRENTWINDOW, button as c_int))
     }
+    /// Types the specified text.
     pub fn type_text(&self, text: &str, delay: Duration) -> OpResult {
         let microsecs = try_microsecs!(delay);
         let string = try!(CString::new(text));
         xdo!(sys::xdo_type(self.handle, sys::CURRENTWINDOW, string.as_ptr(), microsecs as u32))
     }
+    /// Does the specified key sequence.
     pub fn key_sequence(&self, sequence: &str, delay: Duration) -> OpResult {
         let microsecs = try_microsecs!(delay);
         let string = try!(CString::new(sequence));
         xdo!(sys::xdo_keysequence(self.handle, sys::CURRENTWINDOW, string.as_ptr(),
                                   microsecs as u32))
     }
+    /// Releases the specified key sequence.
     pub fn key_sequence_up(&self, sequence: &str, delay: Duration) -> OpResult {
         let microsecs = try_microsecs!(delay);
         let string = try!(CString::new(sequence));
         xdo!(sys::xdo_keysequence_up(self.handle, sys::CURRENTWINDOW, string.as_ptr(),
                                      microsecs as u32))
     }
+    /// Presses the specified key sequence down.
     pub fn key_sequence_down(&self, sequence: &str, delay: Duration) -> OpResult {
         let microsecs = try_microsecs!(delay);
         let string = try!(CString::new(sequence));

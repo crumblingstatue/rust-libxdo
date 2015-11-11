780272753205ea8e9ac497388b7e9a2d6ab961ec
Mika Attila <radiantstatue@gmail.com>
2015-11-12 00:32:56 +0100

    Use sleep instead of sleep_ms

--- examples/autoclicker.rs
+++ examples/autoclicker.rs
@@ -1,12 +1,13 @@
 extern crate libxdo;
 
 use libxdo::XDo;
+use std::time::Duration;
 
 fn main() {
     let xdo = XDo::new(None).unwrap();
 
     for _ in 0..5 {
         xdo.click(1).unwrap();
-        std::thread::sleep_ms(1000);
+        std::thread::sleep(Duration::from_secs(1));
     }
 }
--- examples/yes.rs
+++ examples/yes.rs
@@ -1,12 +1,13 @@
 extern crate libxdo;
 
 use libxdo::XDo;
+use std::time::Duration;
 
 fn main() {
     let xdo = XDo::new(None).unwrap();
     for _ in 0..10 {
         xdo.send_keysequence("y", 0).unwrap();
         xdo.send_keysequence("Return", 0).unwrap();
-        std::thread::sleep_ms(100);
+        std::thread::sleep(Duration::from_millis(100));
     }
 }
